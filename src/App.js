95d921763f9742a2157da254ad9cbd0d357a73f7
zhafira amanda <firaamanda1203@gmail.com>
2024-05-28 15:58:48 +0700

    Added React Router DOM to App component for navigation of other pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BookingForm from "./components/BookingForm";
 import FlightDetails from "./components/FlightDetails";
-import "./index.css";
 import Navbar from "./components/Navbar";
+import PaymentOptions from "./components/PaymentOptions";
+import PaymentSuccess from "./components/PaymentSuccess";
+import "./index.css";
 
 function App() {
   const initialClass = "Economy";
 
   return (
-    <div className="App">
-      <Navbar />
-      <main className="mt-6 flex flex-col md:flex-row gap-2 justify-center">
-        <div className="w-full md:w-1/2">
-          <BookingForm initialClass={initialClass} />
-        </div>
-        <FlightDetails />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/booking"
+            element={
+              <main className="mt-6 flex flex-col md:flex-row gap-2 justify-center">
+                <div className="w-full md:w-1/2">
+                  <BookingForm initialClass={initialClass} />
+                </div>
+                <FlightDetails />
+              </main>
+            }
+          />
+          <Route path="/payment" element={<PaymentOptions />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />{" "}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
