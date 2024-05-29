67942a7b11c248fd80cf86d57cbbd4a3f23f55a3
zhafira amanda <firaamanda1203@gmail.com>
2024-05-29 16:51:43 +0700

    Modify: Update App.js with improved routing and component structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import BookingForm from "./components/BookingForm";
-import FlightDetails from "./components/FlightDetails";
-import Navbar from "./components/Navbar";
-import PaymentOptions from "./components/PaymentOptions";
-import PaymentSuccess from "./components/PaymentSuccess";
+import BookingPage from "./pages/BookingPage";
+import PaymentPage from "./pages/PaymentPage";
+import SuccessPage from "./pages/SuccessPage";
 import "./index.css";
 
 function App() {
-  const initialClass = "Economy";
-
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route
-            path="/booking"
-            element={
-              <main className="mt-6 flex flex-col md:flex-row gap-2 justify-center">
-                <div className="w-full md:w-1/2">
-                  <BookingForm initialClass={initialClass} />
-                </div>
-                <FlightDetails />
-              </main>
-            }
-          />
-          <Route path="/payment" element={<PaymentOptions />} />
-          <Route path="/payment-success" element={<PaymentSuccess />} />{" "}
+          <Route path="/booking" element={<BookingPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/payment-success" element={<SuccessPage />} />
         </Routes>
       </div>
     </Router>
