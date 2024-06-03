0877f64cbff1e067d38d5d04d38b35d7adb4ed7b
zhafira amanda <firaamanda1203@gmail.com>
2024-06-03 15:25:22 +0700

    Connect SignUp  component with API for user register

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,6 +4,11 @@ import plantsImage from "../images/plants.png";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const SignUp = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
 
@@ -11,9 +16,42 @@ const SignUp = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-  const handleSignUp = (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
-    navigate("/sign-in");
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://plucky-agent-424606-s3.et.r.appspot.com/api/v1/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            full_name: fullName,
+            email: email,
+            phone_number: phoneNumber,
+            password: password,
+            confirm_password: confirmPassword,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Registration failed");
+      }
+
+      alert("Registration successful! Please log in.");
+      navigate("/otp");
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   const handleSignIn = () => {
@@ -41,6 +79,8 @@ const SignUp = () => {
               <label className="block text-gray-700 font-bold">Full Name</label>
               <input
                 type="text"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
                 placeholder="Full Name"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-customBlue2"
               />
@@ -49,15 +89,18 @@ const SignUp = () => {
               <label className="block text-gray-700 font-bold">Email</label>
               <input
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-customBlue2"
               />
             </div>
-
             <div className="mb-4">
               <label className="block text-gray-700 font-bold">Phone</label>
               <input
                 type="tel"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
                 placeholder="Phone"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-customBlue2"
               />
@@ -67,7 +110,34 @@ const SignUp = () => {
               <div className="relative">
                 <input
                   type={passwordVisible ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   placeholder="Password"
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-customBlue2"
+                />
+                <div
+                  className="absolute inset-y-0 right-0 pr-3 mt-1 flex items-center cursor-pointer text-gray-500"
+                  onClick={togglePasswordVisibility}
+                  style={{ top: "50%", transform: "translateY(-50%)" }}
+                >
+                  {passwordVisible ? (
+                    <AiFillEyeInvisible size={24} />
+                  ) : (
+                    <AiFillEye size={24} />
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold">
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="Confirm Password"
                   className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-customBlue2"
                 />
                 <div
