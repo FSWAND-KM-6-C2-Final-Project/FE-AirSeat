c912cd2b468813f995f0893941169964e62f294b
itsmegita <gitamarendes@gmail.com>
2024-06-19 09:20:47 +0800

    add filter and search on notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -7,59 +7,36 @@ import { IoNotificationsCircleSharp } from "react-icons/io5";
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([]);
-  const [userId, setUserId] = useState(null);
-  const [notificationClicked, setNotificationClicked] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const notificationsPerPage = 5;
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filterDropdownVisible, setFilterDropdownVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchVisible, setSearchVisible] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    fetch(
-      "https://plucky-agent-424606-s3.et.r.appspot.com/api/v1/notification",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    )
-      .then((response) => {
+    const fetchNotifications = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await fetch(
+          "https://plucky-agent-424606-s3.et.r.appspot.com/api/v1/notification",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch notifications");
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
+        const data = await response.json();
         setNotifications(data.data.notification);
-
-        if (data.data.notification.length > 0) {
-          const fetchedUserId = data.data.notification[0].user_id;
-          setUserId(fetchedUserId);
-
-          const storedClickedStatus = JSON.parse(
-            localStorage.getItem(`notificationClicked_${fetchedUserId}`)
-          );
-          if (storedClickedStatus) {
-            setNotificationClicked(storedClickedStatus);
-          }
-        }
-      })
-      .catch((error) => console.error("Error fetching notifications:", error));
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+      }
+    };
+    fetchNotifications();
   }, []);
-
-  useEffect(() => {
-    if (userId !== null) {
-      localStorage.setItem(
-        `notificationClicked_${userId}`,
-        JSON.stringify(notificationClicked)
-      );
-    }
-  }, [notificationClicked, userId]);
-
-  const handleClick = (index, notificationId) => {
-    const updatedClickedStatus = [...notificationClicked];
-    updatedClickedStatus[index] = true;
-    setNotificationClicked(updatedClickedStatus);
-  };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -87,40 +64,126 @@ const Notification = () => {
     }${minute}`;
   };
 
+  const filteredNotifications = notifications.filter((notification) => {
+    const matchesCategory =
+      selectedCategory === "All" ||
+      notification.notification_type.toLowerCase() ===
+        selectedCategory.toLowerCase();
+    const matchesSearch =
+      notification.notification_title
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase()) ||
+      notification.notification_description
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase());
+    return matchesCategory && matchesSearch;
+  });
+
+  const indexOfLastNotification = currentPage * notificationsPerPage;
+  const indexOfFirstNotification =
+    indexOfLastNotification - notificationsPerPage;
+  const currentNotifications = filteredNotifications.slice(
+    indexOfFirstNotification,
+    indexOfLastNotification
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+    setFilterDropdownVisible(false);
+  };
+
   return (
     <>
       <div className="p-10 shadow-md h-44 mx-auto mb-10">
-        <h2 className="font-bold text-xl text-left md:ml-16 lg:ml-16 xl:ml-40">
+        <h2
+          className={`font-bold text-xl text-left ml-6 sm:ml-4 md:ml-12 lg:ml-16 xl:ml-32 2xl:ml-40 ${
+            searchVisible
+              ? "transform translate-x-[-45px] sm:translate-x-[-35px] md:translate-x-[-60px] lg:translate-x-[-80px] xl:translate-x-[-135px] transition-transform duration-300"
+              : ""
+          }`}
+        >
           Notifikasi
         </h2>
-        <div className="flex justify-center md:justify-start">
+        <div
+          className={`flex justify-center md:justify-start ${
+            searchVisible
+              ? "transform translate-x-[-65px] lg:translate-x-[-80px] xl:translate-x-[-125px] transition-transform duration-300"
+              : ""
+          }`}
+        >
           <Link
-            to={"/"}
-            className="flex items-center bg-customBlue2 text-white px-4 py-3 rounded-xl w-full mt-8 ml-4 md:w-8/12 md:ml-20 lg:ml-20 xl:ml-44 hover:bg-customBlue1"
+            to="/"
+            className="flex items-center bg-customBlue2 text-white px-4 py-3 rounded-xl w-1/10 sm:w-1/2 mt-8 md:w-6/12 lg:w-8/12 md:ml-20 lg:ml-20 xl:ml-36 2xl:ml-44 hover:bg-customBlue1"
           >
             <FiArrowLeft size={24} className="mr-2" />
             Beranda
           </Link>
-          <button
-            className="flex items-center outline outline-customBlue2 px-4 py-1 rounded-full ml-6 mt-10"
-            style={{ height: "fit-content" }}
-          >
-            <BiFilterAlt className="text-gray-400" size={24} />
-            <span className="ml-2">Filter</span>
-          </button>
-          <AiOutlineSearch
-            className="flex items-center mt-10 ml-4 text-customBlue2 shrink-0"
-            size={32}
-          />
+          <div className="relative inline-block text-left ml-6 mt-10">
+            <button
+              className="flex items-center outline outline-customBlue2 px-4 py-1 rounded-full"
+              style={{ height: "fit-content" }}
+              onClick={() => setFilterDropdownVisible(!filterDropdownVisible)}
+            >
+              <BiFilterAlt className="text-gray-500" size={24} />
+              <span className="ml-2">Filter</span>
+            </button>
+            {filterDropdownVisible && (
+              <div className="absolute right-0 mt-2 w-56 origin-top-right bg-white border border-gray-300 rounded-md shadow-lg">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="options-menu"
+                >
+                  <button
+                    onClick={() => handleCategoryChange("All")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  >
+                    All
+                  </button>
+                  <button
+                    onClick={() => handleCategoryChange("notifikasi")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  >
+                    Notifikasi
+                  </button>
+                  <button
+                    onClick={() => handleCategoryChange("promosi")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  >
+                    Promosi
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="relative ml-4 mt-10">
+            <AiOutlineSearch
+              className="flex items-center text-customBlue2 cursor-pointer"
+              size={32}
+              onClick={() => setSearchVisible(!searchVisible)}
+            />
+            {searchVisible && (
+              <input
+                type="text"
+                className="absolute top-0 left-10 border border-customBlue2 rounded-md px-4 py-1 w-32 md:w-36 xl:w-44 2xl:w-60"
+                placeholder="Search..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            )}
+          </div>
         </div>
       </div>
 
-      {notifications.map((notification, index) => (
-        <div key={index}>
+      {currentNotifications.map((notification, index) => (
+        <div key={notification.id || index}>
           <div
             className="flex justify-between mt-2 mb-2 mx-auto w-10/12 md:w-8/12 pb-4 rounded-sm hover:bg-gray-50"
-            onClick={() => handleClick(index, notification.id)}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "default" }}
           >
             <div className="flex">
               <IoNotificationsCircleSharp
@@ -139,18 +202,41 @@ const Notification = () => {
             </div>
             <div className="flex shrink-0">
               <p>{formatDate(notification.created_at)}</p>
-              {notificationClicked[index] ? (
-                <div className="w-4 h-4 bg-green-500 rounded-full ml-2 mt-1"></div>
-              ) : (
-                <div className="w-4 h-4 bg-red-500 rounded-full ml-2 mt-1"></div>
-              )}
             </div>
           </div>
-          {index !== notifications.length - 1 && (
+          {index !== currentNotifications.length - 1 && (
             <hr className="w-10/12 md:w-8/12 mx-auto border-t border-gray-300" />
           )}
         </div>
       ))}
+
+      <div className="flex justify-center mt-2 pb-4">
+        <nav>
+          <ul className="flex list-none">
+            {Array.from(
+              {
+                length: Math.ceil(
+                  filteredNotifications.length / notificationsPerPage
+                ),
+              },
+              (_, index) => (
+                <li key={index} className="mx-1">
+                  <button
+                    onClick={() => paginate(index + 1)}
+                    className={`px-3 py-1 rounded-full ${
+                      currentPage === index + 1
+                        ? "bg-customBlue2 text-white"
+                        : "bg-gray-200 text-gray-700 hover:bg-customBlue3 hover:text-white"
+                    }`}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
+      </div>
     </>
   );
 };
