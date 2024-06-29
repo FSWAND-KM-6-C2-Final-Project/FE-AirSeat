d80282f15e8f188e8e532bdea1a647cefe5e3db2
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-06-29 20:52:23 +0700

    Update file pdf name

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -1164,7 +1164,7 @@ const OrderHistory = () => {
                               bookingDetail={selectedOrder.bookingDetail}
                             />
                           }
-                          fileName={`Invoice_${selectedOrder.booking_code}.pdf`}
+                          fileName={`Airseat_Invoice_${selectedOrder.booking_code}.pdf`}
                         >
                           <button className="text-white w-full bg-customBlue2 hover:bg-customBlue1 focus:ring-blue-300 focus:ring-4 h-14 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 focus:outline-none">
                             Print E-Ticket
